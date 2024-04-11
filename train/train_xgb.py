d411a78c7575b42d94a7f49452704e96ed84e550
Hu <catscarf98@gmail.com>
2024-04-11 15:55:18 +0800

    solve conflict

--- train/train_xgb.py
+++ train/train_xgb.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-=======
-"""
-pip install xgboost
-"""
-
 import xgboost
-from sklearn.metrics import roc_auc_score, f1_score
-from sklearn.model_selection import train_test_split
->>>>>>> b736e63d4e9ed8d40b4eba1d92ae06ef97ac197a
-
 
 import util
 
 def train_xgb():
-    import xgboost
     # 加载数据
     x_train, x_test, y_train, y_test = util.load_onlinefood_splited()
 
@@ -35,7 +24,6 @@ def train_xgb():
 
     # 评估
     yhat_test = model.predict_proba(x_test)[:,1]
-<<<<<<< HEAD
     result = util.evaluate(y_test, yhat_test)
 
     # 保存
@@ -43,16 +31,5 @@ def train_xgb():
     yhat = model.predict_proba(x)[:,1] > 0.5
     util.save(yhat, result + '.txt')
 
-=======
-    auc = roc_auc_score(y_test, yhat_test)
-    f1 = f1_score(y_test>0.5, yhat_test>0.5, average='macro')
-    print(f"auc: {auc:.2f}, f1: {f1:.2f}")
-
-    # 保存模型为json
-    path = f"xgb_auc_{auc:.2f}_f1_{f1:.2f}.json"
-    model.save_model(path)
-    print(f'model saved to {path}')
-    
->>>>>>> b736e63d4e9ed8d40b4eba1d92ae06ef97ac197a
 if __name__ == '__main__':
     train_xgb()
